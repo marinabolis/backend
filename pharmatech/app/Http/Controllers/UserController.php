540020bfdb06b8b51bd0397069f205a64d18a565
marinabolis <marinabahgatbolis25@outlook.com>
2022-03-07 16:25:07 +0200

    register

---
+++ pharmatech/app/Http/Controllers/UserController.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facedes\Hash;
+use App\Models\User;
+// use App\User;
+
+use Tymon\JWTAuth\Facedes\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTExceptions;
+
+
+
+
+
+// use App\Http\Controllers\Log;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // public function index()
+    // {
+    //     //
+    // }
+
+
+// ************ register *******************
+
+    public function register(Request $request)
+    {
+        $user = User ::where('email',$request['email'])->first();
+        if($user)
+            
+      {  
+        $response['status']=0;
+        $response['message']='email already exists';
+        $response['code']=409;
+       }
+       //if not exists 
+        else{
+            $user = User ::create([
+                'name' =>$request-> name,
+                'email'=>$request-> email,
+                'password'=>bcrypt($request->password),
+                'city'=>$request-> city,
+                'street'=>$request->street,
+                'role'=>$request->role
+               
+            ]);
+    
+            $response['status']=1;
+            $response['message']='done';
+            $response['code']=200;
+        }
+       
+        return response()->json($response);
+        
+    }
+
+
+   // ************ login *******************
+
+    public function login(Request $request)
+    {
+     $credentials = $request->only('email','password');
+        try{
+            if(!JWTAuth::attempt($credentials))
+       
+            {
+                $response['status']=0;
+                $response['code']=401;
+                $response['data']=null;
+                $response['message']='email or password is incoreect';
+                return response()->json($response);
+            }
+
+        } catch(JWTExceptions $e){
+            $response['data']=null;
+            $response['code']=500;
+            $response['message']='could Not create Token';
+            return response()->json($response);
+        }
+        
+        $user = auth()->user();
+        $data['token']=auth()->claims([
+            'user_id' => $user-> id,
+            'email' => $user-> email
+        ])->attempt($credentials);
+
+        $response['data']= $data;
+        $response['status']=1;
+        $response['code']=200;
+        $response['message']='login successfully';
+        return response()->json($response);
+
+    }
+
+    // public function login(Request $request)
+    // {
+    //     // grab credentials from the request
+    //     $credentials = $request->only('username', 'password');
+    //     Log::info("username and password obtained from Request: ".json_encode($credentials));
+    
+    //     try
+    //     {
+    //         if(JWTAuth::attempt($credentials)) // For Logging
+    //         {
+    //             Log::info("$ token = JWTAuth::attempt $ credentials Result: TRUE");
+    //         }else{
+    //             Log::info("$ token = JWTAuth::attempt $ credentials Result: FALSE");
+    //         }
+    
+    //         // attempt to verify the credentials and create a token for the user
+    //         if (! $token = JWTAuth::attempt($credentials))
+    //         {
+    //             return response()->json(['error' => 'invalid_credentials'], 401);
+    //         }
+    //     } catch (JWTException $e) {
+    //         // something went wrong whilst attempting to encode the token
+    //         return response()->json(['error' => 'could_not_create_token'], 500);
+    //     }
+    
+    //     Log::info("Generated token is: ".json_encode(compact('token')));
+    //     // all good so return the token
+    //     return response()->json(compact('token'));
+    // }
+    
+
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // public function create()
+    // {
+    //     //
+    // }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // public function show($id)
+    // {
+    //     //
+    // }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // public function edit($id)
+    // {
+    //     //
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    // public function update(Request $request, $id)
+    // {
+    //     //
+    // }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function destroy($id)
+    // {
+    //     //
+    // }
+}
--- pharmatech/config/app.php
+++ pharmatech/config/app.php
@@ -161,6 +161,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+       
+        
+        
+        
 
         /*
          * Package Service Providers...
@@ -175,6 +179,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+      
+        // Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ],
 
     /*
@@ -229,6 +236,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
 
     ],
 
